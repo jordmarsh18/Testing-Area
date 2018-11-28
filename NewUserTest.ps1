6e4d23abf332366b649207e9c2eeda12bb4fe0ec
Jordan <41572183+jordmarsh18@users.noreply.github.com>
2018-11-28 16:45:47 +0000

    Update NewUserTest.ps1

--- NewUserTest.ps1
+++ NewUserTest.ps1
@@ -6,77 +6,80 @@ Creates new Office 365 User in tenant. Users are created via Script User input v
 Should the user already exist, then the the script will change the UserPrincipalName to allow creation of the new user. 
 Following this, the script user will then be prompted to confirm that they wish for the user to be created, and can cancel if desired.
 Following creation of the user, password setup and license assignment will still need to be undertaken via the Admin Centre.
+
 .Example
 New-MsolUser
 
 This creates a new O365 user on the tenant.
 #>
 
+
 $Credential = Get-Credential
 Connect-MsolService -Credential $Credential
 
-$FirstName = Read-Host "Enter in the first name"
+$Firstname = Read-Host "Enter the First Name"
 Write-Host
-$LastName = Read-Host "Enter in the Last Name"
+$LastName = Read-Host "Enter the Last Name"
 Write-Host
-$DisplayName = $FirstName + " " + $LastName
+$Displayname = $Firstname + " " + $LastName
 
 #Acquire domain from Get-MSolDomain command and filter out onmicrosoft.com
 #If a domain is not found, it will revert to the onmicrosoft.com domain
 
-$Domain = Get-MsolDomain | Where-Object {($_.name -notmatch "onmicrosoft.com") -and ($_.status -eq "Verified") -and ($_.Authentication -eq "Managed")} |
-    Select-Object -ExpandProperty name 
-if (-not $Domain) {
-    $domain = Get-MsolDomain | Where-Object {($_.name -match "onmicrosoft.com") -and ($_.status -eq "verified") -and ($_.Authentication -eq "Managed")} |
-        Select-Object -ExpandProperty name 
+$Domain = Get-MsolDomain | Where-Object {($_.Name -notmatch ".onmicrosoft.com") -and ($_.Status -eq "Verified") -and ($_.Authentication -eq "Managed")} | Select-Object -ExpandProperty Name
+if (-not $Domain) {$Domain = Get-MsolDomain | Where-Object {($_.Name -match ".onmicrosoft.com") -and ($_.Status -eq "Verified") -and ($_.Authentication -eq "Managed")} | Select-Object -ExpandProperty Name}
+ 
+$UserPrincipalName = $FirstName + "." + $LastName + "@$Domain"
 
-    $UserPrincipalName = $FirstName + ".$LastName" + "@$Domain"
+Write-Host "First Name: $Firstname"
+Write-Host "Last Name: $LastName"
+Write-Host "DisplayName: $Displayname"
+Write-Host "UserName: $UserPrincipalName"
+Write-Host "Domain: $Domain"
+
+#Check if an O365 account already exists. If so, account creation process will cease.
+
+Do {
+    if ([bool] (Get-MsolUser -UserPrincipalName $UserPrincipalName -ErrorAction SilentlyContinue)) {
+        Write-Host "Login name" $UserPrincipalName.ToUpper() "already exists - please review!" -ForegroundColor Yellow
+        $UserPrincipalName = $FirstName + $LastName + "@$Domain"
+        Write-Host
+        Write-Host "Changing Login name to" $UserPrincipalName.toUpper() -ForegroundColor Yellow
+        Write-Host
+        $Taken = $true
     
-
-    Write-Host "First Name: $FirstName"
-    Write-Host "Last Name: $LastName"
-    Write-Host "Display Name: $DisplayName"
-    Write-Host "User Name: $UserPrincipalName"
-    Write-Host "Domain: $Domain"
-
-    #Check if an O365 account already exists. If so, account creation process will cease.
-
-    Do {
-        if ([bool] (Get-MsolUser -UserPrincipalName $UserPrincipalName -ErrorAction SilentlyContinue)) {
-            Write-Host "Login name" $UserPrincipalName.ToUpper() "already exists - please review!" -ForegroundColor Yellow
-            $UserPrincipalName = $FirstName + $LastName + "@$Domain"
-            Write-Host
-            Write-Host "Changing Login name to" $UserPrincipalName.toUpper() -ForegroundColor Yellow
-            Write-Host
-            $Taken = $true
-    
-        }
-        else {
-            $taken = $false
-        }
-    } Until ($taken -eq $false)
-    $UserPrincipalName = $UserPrincipalName.ToLower()
-
-    write-host "_______________________"
-    Write-Host "First Name: $FirstName"
-    Write-Host "Last Name: $LastName"
-    Write-Host "Display Name: $DisplayName"
-    WRite-host "User Name: $UserPrincipalName"
-    Write-Host "Domain: $Domain"
-    Write-Host
-
-    Write-Host "Thank you - User will now be created." -ForegroundColor Green
-    $Proceed = $null
-    $Proceed = Write-Host "Proceed Y/N?"
-
-    if ($Proceed -ieq 'Y') {
-        New-MsolUser -DisplayName $DisplayName -FirstName $FirstName -LastName -UserPrincipalName $UserPrincipalName -Password $Password
-        Get-MsolUser -UserPrincipalName $UserPrincipalName
-        Write-Host "Process complete - User $DisplayName has now been created" -ForegroundColor Green
-    
-        #The account should now be appearing under Active Users in the O365 Admin Centre
     }
     else {
-        Write-Host "Process cancelled"
+        $taken = $false
     }
+} Until ($taken -eq $false)
+$UserPrincipalName = $UserPrincipalName.ToLower()
+
+Write-Host "____________________________________________________"
+Write-Host
+Write-Host "First Name: $Firstname"
+Write-Host "Last Name: $LastName"
+Write-Host "DisplayName: $Displayname"
+Write-Host "UserName: $UserPrincipalName"
+Write-Host "Domain: $Domain"
+Write-Host 
+
+Write-Host "Thank you - User will now be created" -ForegroundColor Green
+
+$Proceed = $null
+$Proceed = Read-Host "Proceed - Y/N?"
+
+if ($Proceed -ieq 'Y') {
+    New-MsolUser -DisplayName $DisplayName -FirstName $FirstName -LastName $LastName -UserPrincipalName $UserPrincipalName
+
+    Get-MsolUser -UserPrincipalName $UserPrincipalName
+
+    Write-Host "Process complete. user $Displayname has now been created" -ForegroundColor Green
+
+    #The account should now be appearing under Active Users in the O365 Admin Centre
+
 }
+else {
+    Write-Host "Process cancelled" -ForegroundColor Yellow
+}
+
