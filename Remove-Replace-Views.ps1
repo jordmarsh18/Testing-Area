c4f1f7a7766d97b1edee7b0ca351a879255d9620
Jordan <41572183+jordmarsh18@users.noreply.github.com>
2020-02-26 13:53:30 +0000

    Update Remove-Replace-Views.ps1

--- Remove-Replace-Views.ps1
+++ Remove-Replace-Views.ps1
@@ -1,6 +1,3 @@
-#Removes old views and recreates them but with added queries.
-#Add-PnPView occasionally needs to be wrapped in a $ variable for some reason, otherwise the command doesn't work?
-
 $sites = Get-UnifiedGroup -Filter {[Alias] -like "BSS-*"}
 
 foreach ($site in $sites) {
@@ -14,22 +11,30 @@ $list = "Employees"
 Write-Output "Removing Views from $list section on site $($site.Alias)"
 Remove-PnPView -List "Employees" -Identity "Active" -Force
 Remove-PnPView -List "Employees" -Identity "Ex-Employee" -Force
-
+Remove-PnPView -List "Employees" -Identity "All Documents" -Force
 
 Write-Output "Adding Active View to $list section on site $($site.Alias)"
-
 $viewTitle = "Active"
 $viewFields = @("Type", "Name", "Enterprise Keywords", "Modified", "Modified By", "Occupation", "Employee Status")
 $viewQuery = "<Where><Eq><FieldRef Name = 'EmployeeStatus' /><Value Type = 'Choice'>Active</Value></Eq></Where>"
 
-$Add1 = Add-PnPView -List $list -Title $viewTitle -Query $viewQuery -Fields $viewFields
+
+$Add1 = Add-PnPView -List $list -Title $viewTitle -Query $viewQuery -Fields $viewFields -rowlimit 5000 
 
 Write-Output "Adding Ex-Employee View to $list section on site $($site.Alias)"
-$list = "Employees"
 $viewTitle = "Ex-Employee"
 $viewFields = @("Type", "Name", "Enterprise Keywords", "Modified", "Modified By", "Occupation", "Employee Status")
 $viewQuery2 = "<Where><Eq><FieldRef Name = 'EmployeeStatus' /><Value Type = 'Choice'>Ex-Employee</Value></Eq></Where>"
 
-$Add2 = Add-PnPView -List $list -Title $viewTitle -Query $viewQuery2 -Fields $viewFields
+$Add2 = Add-PnPView -List $list -Title $viewTitle -Query $viewQuery2 -Fields $viewFields -rowlimit 5000
+
+
+
+Write-Output "Adding All Documents View to $list section on site $($site.Alias)"
+$viewTitle = "All Documents"
+$viewFields = @("Type", "Name", "Enterprise Keywords", "Modified", "Modified By", "Occupation", "Employee Status")
+
+$Add3 = Add-PnPView -List $list -Title $viewTitle -Fields $viewFields -rowlimit 5000 -SetasDefault
+
 
 }#end foreach site
