5ed66c505092a982988009a2cc452aa3ac19620f
Jordan <41572183+jordmarsh18@users.noreply.github.com>
2020-03-06 16:12:12 +0000

    Update Remove-Replace-Views.ps1

--- Remove-Replace-Views.ps1
+++ Remove-Replace-Views.ps1
@@ -1,3 +1,6 @@
+#Below script used for FA Employee Folders - Creates Active/Ex-Employee Views, filtering on the Employee Status column, and also displays folder items alphabetically.
+
+
 $sites = Get-UnifiedGroup -Filter {[Alias] -like "BSS-*"}
 
 foreach ($site in $sites) {
@@ -16,7 +19,7 @@ Remove-PnPView -List "Employees" -Identity "All Documents" -Force
 Write-Output "Adding Active View to $list section on site $($site.Alias)"
 $viewTitle = "Active"
 $viewFields = @("Type", "Name", "Enterprise Keywords", "Modified", "Modified By", "Occupation", "Employee Status")
-$viewQuery = "<Where><Eq><FieldRef Name = 'EmployeeStatus' /><Value Type = 'Choice'>Active</Value></Eq></Where>"
+$viewQuery = "<Where><Eq><FieldRef Name = 'EmployeeStatus' /><Value Type = 'Choice'>Active</Value></Eq></Where><OrderBy><FieldRef Name='LinkFilename' Ascending='TRUE'/></OrderBy>"
 
 
 $Add1 = Add-PnPView -List $list -Title $viewTitle -Query $viewQuery -Fields $viewFields -rowlimit 5000 
@@ -24,7 +27,7 @@ $Add1 = Add-PnPView -List $list -Title $viewTitle -Query $viewQuery -Fields $vie
 Write-Output "Adding Ex-Employee View to $list section on site $($site.Alias)"
 $viewTitle = "Ex-Employee"
 $viewFields = @("Type", "Name", "Enterprise Keywords", "Modified", "Modified By", "Occupation", "Employee Status")
-$viewQuery2 = "<Where><Eq><FieldRef Name = 'EmployeeStatus' /><Value Type = 'Choice'>Ex-Employee</Value></Eq></Where>"
+$viewQuery2 = "<Where><Eq><FieldRef Name = 'EmployeeStatus' /><Value Type = 'Choice'>Ex-Employee</Value></Eq></Where><OrderBy><FieldRef Name='LinkFilename' Ascending='TRUE'/></OrderBy>"
 
 $Add2 = Add-PnPView -List $list -Title $viewTitle -Query $viewQuery2 -Fields $viewFields -rowlimit 5000
 
@@ -33,8 +36,9 @@ $Add2 = Add-PnPView -List $list -Title $viewTitle -Query $viewQuery2 -Fields $vi
 Write-Output "Adding All Documents View to $list section on site $($site.Alias)"
 $viewTitle = "All Documents"
 $viewFields = @("Type", "Name", "Enterprise Keywords", "Modified", "Modified By", "Occupation", "Employee Status")
+$viewQuery3 = "<OrderBy><FieldRef Name='LinkFilename' Ascending='TRUE'/></OrderBy>"
 
-$Add3 = Add-PnPView -List $list -Title $viewTitle -Fields $viewFields -rowlimit 5000 -SetasDefault
+$Add3 = Add-PnPView -List $list -Title $viewTitle -Fields $viewFields -Query $viewQuery3 -rowlimit 5000 -SetasDefault
 
 
 }#end foreach site
